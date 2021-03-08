1ca5685f706abcaba049c4a9d0be50964dcb8e46
Paul Caine <paulcaine@tutanota.com>
2021-03-08 09:23:20 +0000

    first commit

---
+++ src/src/pages/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+// Styling
+import "../scss/style.scss"
+
+ const index = () => {
+  return (
+    <main>
+      <div id="card">
+        <div id="top"></div>
+        <div id="mid">
+          
+        </div>
+      </div>
+    </main>
+  )
+}
+
+export default index
